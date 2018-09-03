c6d47c2c7bd23e15a1215f9dd8c9619be623de15
Ryand9010 <ryandavis0@gmail.com>
2018-09-03 16:53:23 -0700

    Added Player Animation

--- Infinite_Runner/Assets/Scripts/CameraFollow.cs
+++ Infinite_Runner/Assets/Scripts/CameraFollow.cs
@@ -5,26 +5,38 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     private GameObject player;
-    public float xMin;
-    public float xMax;
-    public float yMin;
-    public float yMax;
+    private Vector3 lastPlayerPos;
+    private float distanceToMove;
+
+    //public float xMin;
+    //public float xMax;
+    //public float yMin;
+    //public float yMax;
 
 
     // Use this for initialization
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        lastPlayerPos = player.transform.position;
+    }
 
+    void Update()
+    {
+        distanceToMove = player.transform.position.x - lastPlayerPos.x;
+
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPos = player.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
-        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        //float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        //float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
 
-        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+        //gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
 
 
     }
--- Infinite_Runner/Assets/Scripts/PlayerScript.cs
+++ Infinite_Runner/Assets/Scripts/PlayerScript.cs
@@ -5,7 +5,31 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour {
 
+    //Animation
+    private Animator myAnimator;
+
+
+    //JUMPING
     public float jumpPower = 3.0f;
+    public float speedMultiplier;
+    public float speedIncreaseMilestone;
+    private float speedMilestonCount;
+    public float jumpTime;
+    private float jumpTimeCounter;
+
+    //Keeps 
+    private bool stoppedJumping;
+
+    private bool canDoubleJump;
+
+    //Collision with side of platform
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    private bool grounded2;
+    private Collider2D myCollider;
+
 
     [SerializeField]
     private float movementSpeed = 10;
@@ -15,20 +39,53 @@ public class PlayerScript : MonoBehaviour {
 
     private GameManager gm;
 
+    public AudioSource jumpSound;
+    public AudioSource deathSound;
+
     // Use this for initialization
     void Start()
     {
         myRigidBody = transform.GetComponent<Rigidbody2D>();
         posX = transform.position.x;
 
+        myCollider = GetComponent<Collider2D>();
+
+        speedMilestonCount = speedIncreaseMilestone; //Making sure the count starts are the 100 mark, instead of starting at 0. 
+        //Gives player a chance to get grounding before speeding up
+
+        stoppedJumping = true;
+
+        canDoubleJump = true;
+
+        // Finding animator attached to the player
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        //Checking to see when the player gets above the count number
+        if(transform.position.x > speedMilestonCount)
+        {
+            speedMilestonCount += speedIncreaseMilestone; //adding the 'set' distance to the current distance to get the new milestone counter
+
+            speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier; //Making 'set' distance increase the farther the player gets
+
+            movementSpeed = movementSpeed * speedMultiplier; //Changing the movement speed
+        }
+
+        myAnimator.SetFloat("Speed", myRigidBody.velocity.x);
+        myAnimator.SetBool("Grounded", grounded);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
-        //float horizontal = Input.GetAxis("Horizontal");
-
+        
         //HandleMovement(horizontal);
         myRigidBody.velocity = new Vector2(movementSpeed, myRigidBody.velocity.y);
         PlayerJump();
@@ -37,46 +94,86 @@ public class PlayerScript : MonoBehaviour {
         if(myRigidBody.position.y < -6.75)
         {
             FindObjectOfType<GameManager>().EndGame();
+
+            deathSound.Play();
         }
 
     }
 
-    ////Horizantal movement
-    //private void HandleMovement(float horizontal)
-    //{
-    //    myRigidBody.velocity =  new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
-    //}
-
+    
     private void PlayerJump()
     {
         //Jump
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
         {
-            myRigidBody.AddForce(Vector3.up * (jumpPower * myRigidBody.mass * myRigidBody.gravityScale * 15));
+            if (grounded)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpPower);
+
+                stoppedJumping = false;
+                jumpSound.Play();
+            }
+            if (!grounded && canDoubleJump == true)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpPower);
+
+                canDoubleJump = false;
+
+                stoppedJumping = false;
+
+                jumpTimeCounter = jumpTime;
+
+                jumpSound.Play();
+            }
+        }
+
+        if ((Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) && stoppedJumping == false)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpPower);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            jumpTimeCounter = 0;
+
+            stoppedJumping = true;
+        }
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+
+            canDoubleJump = true;
         }
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.collider.tag == "Ground")
+        if (other.collider.tag == "Obstacle")
         {
-            isGrounded = true;
+            FindObjectOfType<GameManager>().EndGame();
+
+            deathSound.Play();
         }
     }
 
-    void OnCollisionStay2D(Collision2D other)
-    {
-        if (other.collider.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-    }
+    //void OnCollisionStay2D(Collision2D other)
+    //{
+    //    if (other.collider.tag == "Ground")
+    //    {
+    //        isGrounded = true;
+    //    }
+    //}
 
-    void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.collider.tag == "Ground")
-        {
-            isGrounded = false;
-        }
-    }
+    //void OnCollisionExit2D(Collision2D other)
+    //{
+    //    if (other.collider.tag == "Ground")
+    //    {
+    //        isGrounded = false;
+    //    }
+    //}
 }
